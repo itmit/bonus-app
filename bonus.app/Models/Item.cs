2834074c7c9aa0b7710a48f843df7f7c08b1ac47
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-10-28 12:53:57 +0300

    Добавьте файлы проекта.

---
+++ bonus.app/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace bonus.app.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
