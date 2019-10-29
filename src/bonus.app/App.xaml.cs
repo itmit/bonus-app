3764f5f40267c30e60523c757dac7b3144259a7d
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-10-29 18:17:55 +0300

    Fix

--- src/bonus.app/App.xaml.cs
+++ src/bonus.app/App.xaml.cs
@@ -10,9 +10,28 @@ namespace bonus.app
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = InitMainTabbedPage();
         }
+        public TabbedPage InitMainTabbedPage()
+        {
+            var tabbedNavigation = new MainTabbedPage(NavigationContainerNames.MainContainer);
 
+            tabbedNavigation.CurrentPage = tabbedNavigation.AddTab<CategoriesPageModel>(null, "ic_action_home.png");
+            tabbedNavigation.AddTab<FavoritesPageModel>(null, "star_2.png");
+            tabbedNavigation.AddTab<RatingPageModel>(null, "ic_action_search.png");
+            tabbedNavigation.AddTab<MenuPageModel>(null, "ic_action_dehaze.png");
+
+            tabbedNavigation.Effects.Add(new NoShiftEffect());
+            tabbedNavigation.On<TabBar.Android>()
+                            .SetToolbarPlacement(ToolbarPlacement.Bottom);
+            tabbedNavigation.On<TabBar.Android>()
+                            .SetIsSwipePagingEnabled(false);
+            tabbedNavigation.BarBackgroundColor = Color.FromHex("#228bcc");
+            tabbedNavigation.SelectedTabColor = Color.Black;
+            tabbedNavigation.UnselectedTabColor = Color.White;
+
+            return tabbedNavigation;
+        }
         protected override void OnStart()
         {
 
---
+++ src/bonus.app/MainTabbedPage.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreshMvvm;
+using Xamarin.Forms;
+
+namespace bonus.app
+{
+    public class MainTabbedPage : TabbedPage, IFreshNavigationService
+    {
+        #region Data
+        #region Fields
+        /// <summary>
+        /// Список страниц отображаемых в меню.
+        /// </summary>
+        private readonly List<Page> _tabs = new List<Page>();
+        #endregion
+        #endregion
+
+        #region .ctor
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="MainTabbedPage" />, регистрируя сервис навигации.
+        /// </summary>
+        public MainTabbedPage(string navigationServiceName)
+        {
+            CurrentPageChanged += CurrentPageHasChanged;
+            NavigationServiceName = navigationServiceName;
+            RegisterNavigation();
+        }
+        #endregion
+
+        #region Overridable
+        /// <summary>
+        /// Добавляет страницу в меню.
+        /// </summary>
+        /// <typeparam name="T">Модель представления страницы.</typeparam>
+        /// <param name="title">Заголовок страницы.</param>
+        /// <param name="icon">Иконка страницы.</param>
+        /// <param name="data">Данные передаваемые в меню.</param>
+        /// <returns>Созданная страница.</returns>
+        public virtual Page AddTab<T>(string title, string icon, object data = null) where T : FreshBasePageModel
+        {
+            var page = FreshPageModelResolver.ResolvePageModel<T>(data);
+            page.GetModel()
+                .CurrentNavigationServiceName = NavigationServiceName;
+            _tabs.Add(page);
+            var navigationContainer = CreateContainerPageSafe(page);
+            navigationContainer.Title = title;
+            if (!string.IsNullOrWhiteSpace(icon))
+            {
+                navigationContainer.IconImageSource = icon;
+            }
+
+            Children.Add(navigationContainer);
+            return navigationContainer;
+        }
+
+        /// <summary>
+        /// Создает страницу навигации.
+        /// </summary>
+        /// <param name="page">Изначальная страница.</param>
+        /// <returns>Страница навигации.</returns>
+        protected virtual Page CreateContainerPage(Page page) => new NavigationPage(page);
+        #endregion
+
+        #region IFreshNavigationService members
+        /// <summary>
+        /// Возвращает название сервиса навигации.
+        /// </summary>
+        public string NavigationServiceName
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Уведомляет потомков об остановке. 
+        /// </summary>
+        public void NotifyChildrenPageWasPopped()
+        {
+            foreach (var page in Children)
+            {
+                if (page is NavigationPage navigationPage)
+                {
+                    navigationPage.NotifyAllChildrenPopped();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Покидает текущую страницу.
+        /// </summary>
+        /// <param name="modal">Покинуть ли страницу модально?</param>
+        /// <param name="animate">Покинуть ли страницу с анимацией?</param>
+        /// <returns>Возвращает операцию.</returns>
+        public Task PopPage(bool modal = false, bool animate = true)
+        {
+            if (modal)
+            {
+                return CurrentPage.Navigation.PopModalAsync(animate);
+            }
+
+            return CurrentPage.Navigation.PopAsync(animate);
+        }
+
+        /// <summary>
+        /// Возвращает к корневой странице.
+        /// </summary>
+        /// <param name="animate">Вернуть ли с анимацией?</param>
+        /// <returns>Возвращает операцию.</returns>
+        public Task PopToRoot(bool animate = true) => CurrentPage.Navigation.PopToRootAsync(animate);
+
+        /// <summary>
+        /// Открывает страницу.
+        /// </summary>
+        /// <param name="page">Открываемая страница.</param>
+        /// <param name="model">Контекст привязки страницы.</param>
+        /// <param name="modal">Открыть ли страницу модально?</param>
+        /// <param name="animate">Открыть ли страницу с анимацией?</param>
+        /// <returns>Возвращает операцию.</returns>
+        public Task PushPage(Page page, FreshBasePageModel model, bool modal = false, bool animate = true)
+        {
+            if (modal)
+            {
+                return CurrentPage.Navigation.PushModalAsync(CreateContainerPageSafe(page));
+            }
+
+            return CurrentPage.Navigation.PushAsync(page);
+        }
+
+        /// <summary>
+        /// Переключает выбранную корневую модель страницы.
+        /// </summary>
+        /// <typeparam name="T">Тип модели представления страницы</typeparam>
+        /// <returns>Возвращает операцию.</returns>
+        public Task<FreshBasePageModel> SwitchSelectedRootPageModel<T>() where T : FreshBasePageModel
+        {
+            var page = _tabs.FindIndex(o => o.GetModel()
+                                             .GetType()
+                                             .FullName ==
+                                            typeof(T).FullName);
+
+            if (page > -1)
+            {
+                CurrentPage = Children[page];
+                var topOfStack = CurrentPage.Navigation.NavigationStack.LastOrDefault();
+                if (topOfStack != null)
+                {
+                    return Task.FromResult(topOfStack.GetModel());
+                }
+            }
+
+            return null;
+        }
+        #endregion
+
+        private void CurrentPageHasChanged(object sender, EventArgs e)
+        {
+            if ((sender as TabbedPage)?.CurrentPage is NavigationPage navigationPage)
+            {
+                if (navigationPage.CurrentPage.BindingContext is BaseMainPageModel pageModel)
+                {
+                    if (pageModel.IsLoaded)
+                    {
+                        return;
+                    }
+                    pageModel.LoadData();
+                }
+            }
+        }
+
+        #region Private
+        /// <summary>
+        /// Создает страницу навигации, с учетом типа страницы.
+        /// </summary>
+        /// <param name="page">Изначальная страница.</param>
+        /// <returns>Страница навигации.</returns>
+        private Page CreateContainerPageSafe(Page page)
+        {
+            if (page is NavigationPage || page is MasterDetailPage || page is TabbedPage)
+            {
+                return page;
+            }
+
+            return CreateContainerPage(page);
+        }
+
+        /// <summary>
+        /// Регистрирует сервис навигации.
+        /// </summary>
+        private void RegisterNavigation()
+        {
+            FreshIOC.Container.Register<IFreshNavigationService>(this, NavigationServiceName);
+        }
+
+        public Task PushPage(Xamarin.Forms.Page page, FreshBasePageModel model, bool modal = false, bool animate = true)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
