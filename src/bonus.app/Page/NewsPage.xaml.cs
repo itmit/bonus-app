127c04c99c0eee3db385349c6ec4678c2fcfb595
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-11-07 14:02:13 +0300

    Страница новостей и начисления

--- src/bonus.app/Page/NewsPage.xaml.cs
+++ src/bonus.app/Page/NewsPage.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using bonus.app.Models;
+using System.Collections.ObjectModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
