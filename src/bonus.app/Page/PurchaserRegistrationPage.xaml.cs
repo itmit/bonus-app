12d61141f2bd96b0ea6e260d3b8714cc36aff108
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-11-12 14:32:36 +0300

    14.2.1 Регистрация

--- src/bonus.app/Page/PurchaserRegistrationPage.xaml.cs
+++ src/bonus.app/Page/PurchaserRegistrationPage.xaml.cs
@@ -10,16 +10,11 @@ using Xamarin.Forms.Xaml;
 namespace bonus.app.Page
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BusinessmanAndBuyerPage : ContentPage
+    public partial class PurchaserRegistrationPage : ContentPage
     {
-        public BusinessmanAndBuyerPage()
+        public PurchaserRegistrationPage()
         {
             InitializeComponent();
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new PurchaserRegistrationPage());
         }
     }
 }
