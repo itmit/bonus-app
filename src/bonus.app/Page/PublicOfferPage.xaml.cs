69389b8d66cddfeef578d04ebeea72d8e0f92b79
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-11-15 13:32:06 +0300

    Публичная оферта и модалка

--- src/bonus.app/Page/PublicOfferPage.xaml.cs
+++ src/bonus.app/Page/PublicOfferPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using bonus.app.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,24 @@ namespace bonus.app.Page
         public PublicOfferPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Добро пожаловать в Bonus &amp; Marketing", "Вы успешно зарегистрировались в приложении Bonus &amp; Marketing и получили личный QR-CODE с помощью которого вы можете получать бонусы", "ОK");
+            Application.Current.MainPage = new MainPage();
+        }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            if(Ch1.IsChecked && Ch2.IsChecked)
+            {
+                ButSave.IsEnabled = true;
+            }
+            else
+            {
+                ButSave.IsEnabled = false;
+            }
         }
     }
 }
