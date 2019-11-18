7ca109867f4a8821e797974ce7a7922a533f4532
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-11-18 14:38:08 +0300

    Акции

---
+++ src/bonus.app/Page/BuyerSharesPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace bonus.app.Page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BuyerSharesPage : ContentPage
+    {
+        public BuyerSharesPage()
+        {
+            InitializeComponent();
+
+            BackgroundColor = Color.FromRgba(196, 196, 196, 0.2);
+        }
+    }
+}
