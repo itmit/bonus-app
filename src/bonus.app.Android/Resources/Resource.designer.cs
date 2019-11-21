9dab0213db4b06f4b03ff49584e54d8c989a03c7
Радик <35137419+Shazlogic@users.noreply.github.com>
2019-11-21 13:29:00 +0300

    Начисление бонусов

--- src/bonus.app.Android/Resources/Resource.designer.cs
+++ src/bonus.app.Android/Resources/Resource.designer.cs
@@ -5806,34 +5806,37 @@ namespace bonus.app.Droid
 			public const int qrcode = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int starShares = 2131165306;
+			public const int qrcodetwo = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int tab_five = 2131165307;
+			public const int starShares = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int tab_four = 2131165308;
+			public const int tab_five = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int tab_one = 2131165309;
+			public const int tab_four = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int tab_three = 2131165310;
+			public const int tab_one = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int tab_two = 2131165311;
+			public const int tab_three = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int tooltip_frame_dark = 2131165312;
+			public const int tab_two = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int tooltip_frame_light = 2131165313;
+			public const int tooltip_frame_dark = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int xamarin_logo = 2131165314;
+			public const int tooltip_frame_light = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int ZnTRZG5o1xE1 = 2131165315;
+			public const int xamarin_logo = 2131165315;
+			
+			// aapt resource value: 0x7F070084
+			public const int ZnTRZG5o1xE1 = 2131165316;
 			
 			static Drawable()
 			{
