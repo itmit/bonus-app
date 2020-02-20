dc7ac5d0ef48f432f206b11288fad461abd98800
fastandfurios <semu-dima@yandex.ru>
2020-02-20 13:27:02 +0300

    переделаны

--- src/bonus.app/Page/ArchiveStockPage.xaml.cs
+++ src/bonus.app/Page/ArchiveStockPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using Xamarin.Forms;
+﻿using bonus.app.Core.ViewModels;
+using MvvmCross.Forms.Views;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace bonus.app.Core.Page
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ArchiveStockPage : ContentPage
+    public partial class ArchiveStockPage : MvxContentPage<ArchiveStockViewModel>
     {
         public ArchiveStockPage()
         {
